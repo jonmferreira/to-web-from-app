aad3b533246c52210becb3ed55d6fbd6c58244b4
Jan <janielson_moura20@outlook.com>
2022-05-22 20:14:04 -0300

    Signed-off-by: Jan <janielson_moura20@outlook.com>

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+import { StyleSheet} from 'react-native';
+import React from 'react';
+import {
+    StatusBar
+} from 'react-native';
+import {NavigationContainer} from "@react-navigation/native";
+import {AuthProvider} from "./src/contexts/auth";
+import Routes from "./src/routes";
+import {colors} from "./src/assets/styles/colors";
+
+
+const Applayout = () => {
+
+  return (
+    <NavigationContainer>
+      <AuthProvider>
+        <Routes/>
+        <StatusBar   backgroundColor={colors.transparent} translucent={true}  barStyle="light-content" />
+      </AuthProvider>
+    </NavigationContainer>
+  );
+};
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Applayout/>
   );
 }
 
